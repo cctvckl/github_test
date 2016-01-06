ca227eff91fc7b3c40012759a85de6229b4c26df
914000408 <914000408@qq.com>
2016-01-06 21:13:40 +0800

    jedis操作redis实例

---
+++ jedis/src/main/java/com/ljq/utils/RedisAPI.java
@@ -0,0 +1,91 @@
+package com.ljq.utils;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.util.SafeEncoder;
+
+/**
+ * Redis操作接口
+ *
+ * @author 林计钦
+ * @version 1.0 2013-6-14 上午08:54:14
+ */
+public class RedisAPI {
+	private static JedisPool pool = null;
+	private static Jedis jedis;
+
+	/**
+	 * 构建redis连接池
+	 * 
+	 * @param ip
+	 * @param port
+	 * @return JedisPool
+	 */
+	public static JedisPool getPool() {
+		if (pool == null) {
+			JedisPoolConfig config = new JedisPoolConfig();
+			// 控制一个pool可分配多少个jedis实例，通过pool.getResource()来获取；
+			// 如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
+			config.setMaxActive(200);
+			// 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
+			config.setMaxIdle(200);
+			// 表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
+			config.setMaxWait(1000 * 100);
+			// 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+			config.setTestOnBorrow(true);
+			pool = new JedisPool(config, "10.11.10.35", 6380);
+		}
+		return pool;
+	}
+
+	/**
+	 * 返还到连接池
+	 * 
+	 * @param pool
+	 * @param redis
+	 */
+	public static void returnResource(JedisPool pool, Jedis redis) {
+		if (redis != null) {
+			pool.returnResource(redis);
+		}
+	}
+
+	/**
+	 * 获取数据
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public static String get(String key) {
+		String value = null;
+
+		try {
+			value = jedis.get(key);
+		} catch (Exception e) {
+			// 释放redis对象
+			pool.returnBrokenResource(jedis);
+			e.printStackTrace();
+		} finally {
+			// 返还到连接池
+			returnResource(pool, jedis);
+		}
+
+		return value;
+	}
+	public static enum Command {
+		PING, SET, GET, QUIT, EXISTS, DEL, TYPE, FLUSHDB, KEYS, RANDOMKEY, RENAME, RENAMENX, RENAMEX, DBSIZE, EXPIRE, EXPIREAT, TTL, SELECT, MOVE, FLUSHALL, GETSET, MGET, SETNX, SETEX, MSET, MSETNX, DECRBY, DECR, INCRBY, INCR, APPEND, SUBSTR, HSET, HGET, HSETNX, HMSET, HMGET, HINCRBY, HEXISTS, HDEL, HLEN, HKEYS, HVALS, HGETALL, RPUSH, LPUSH, LLEN, LRANGE, LTRIM, LINDEX, LSET, LREM, LPOP, RPOP, RPOPLPUSH, SADD, SMEMBERS, SREM, SPOP, SMOVE, SCARD, SISMEMBER, SINTER, SINTERSTORE, SUNION, SUNIONSTORE, SDIFF, SDIFFSTORE, SRANDMEMBER, ZADD, ZRANGE, ZREM, ZINCRBY, ZRANK, ZREVRANK, ZREVRANGE, ZCARD, ZSCORE, MULTI, DISCARD, EXEC, WATCH, UNWATCH, SORT, BLPOP, BRPOP, AUTH, SUBSCRIBE, PUBLISH, UNSUBSCRIBE, PSUBSCRIBE, PUNSUBSCRIBE, ZCOUNT, ZRANGEBYSCORE, ZREVRANGEBYSCORE, ZREMRANGEBYRANK, ZREMRANGEBYSCORE, ZUNIONSTORE, ZINTERSTORE, SAVE, BGSAVE, BGREWRITEAOF, LASTSAVE, SHUTDOWN, INFO, MONITOR, SLAVEOF, CONFIG, STRLEN, SYNC, LPUSHX, PERSIST, RPUSHX, ECHO, LINSERT, DEBUG, BRPOPLPUSH, SETBIT, GETBIT, SETRANGE, GETRANGE, EVAL, EVALSHA, SCRIPT, SLOWLOG, OBJECT;
+
+	    }
+	public static void main(String[] args) {
+		pool = getPool();
+		jedis = pool.getResource();
+		jedis.set("name", "ckl..");
+		System.out.println(get("name"));
+		jedis.del("name");
+		System.out.println(get("name"));
+		
+		Command a = Command.BGSAVE;
+		System.out.println(a.toString());
+	}
+}
